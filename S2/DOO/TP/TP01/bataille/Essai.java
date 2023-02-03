4c9fc519aa3dcf3245e994b9055dc18cac16cbe2
Amiiine27 <amineakh213@outlook.fr>
2023-02-03 14:46:51 +0100

    Ajout des 2 cartes

--- S2/DOO/TP/TP01/bataille/Essai.java
+++ S2/DOO/TP/TP01/bataille/Essai.java
@@ -1,10 +1,18 @@
 package bataille;
 
+import java.util.ArrayList;
+
 public class Essai {
 
     public static void main(String[] args) {
+        Carte C1, C2, C3;
+        C1 = new Carte(10, "coeur");
+        C2 = new Carte(7, "trèfle");
 
+        System.out.println("C1 = " + C1.toString());
+        System.out.println("C2 = " + C2.toString());
     }
+
 
     
 }
