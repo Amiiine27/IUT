3652bc1095b50d4a6fa96777cb8802d4ea4593a7
Amiiine27 <amineakh213@outlook.fr>
2023-02-03 15:00:22 +0100

    ajout paquet battu

--- S2/DOO/TP/TP01/bataille/Essai.java
+++ S2/DOO/TP/TP01/bataille/Essai.java
@@ -12,10 +12,15 @@ public class Essai {
         System.out.println("C1 = " + C1.toString());
         System.out.println("C2 = " + C2.toString());
 
-        ArrayList<Carte> paquet;
+        ArrayList<Carte> paquetTrié;
 
-        paquet = FabriqueDeJeuxDe32Cartes.créerJeu32DansOrdre();
-        System.out.println(paquet);
+        paquetTrié = FabriqueDeJeuxDe32Cartes.créerJeu32DansOrdre();
+        System.out.println(paquetTrié);
+
+        ArrayList<Carte> paquetBattu;
+
+        paquetBattu = FabriqueDeJeuxDe32Cartes.créerJeu32Battu();
+        System.out.println(paquetBattu);
     }
 
 
