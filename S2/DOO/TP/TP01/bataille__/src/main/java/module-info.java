b7164df6e4a12db1f745149cdf5919dee0e15af3
Amiiine27 <amineakh213@outlook.fr>
2023-02-03 22:42:58 +0100

    retirer bataille__

--- S2/DOO/TP/TP01/bataille__/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bataille__ {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bataille__ to javafx.fxml;
-    exports com.example.bataille__;
-}
